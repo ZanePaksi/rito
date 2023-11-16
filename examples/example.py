bc953038a0af01a73a6ad33c03b2fc0428d91554
ZanePaksi <zane.paksi@outlook.com>
2023-11-16 15:46:39 -0500

    updated readme with badges and examples

--- examples/example.py
+++ examples/example.py
@@ -1,18 +1,16 @@
-from rito import two_rito
+from rito import rito
 import json
 from datetime import datetime
 
 
 # Here's a quick example of utilizing the validator and outputting the result to a txt file
 def main():
-    validator = two_rito.Validator('r4')
-    # resource = open("../test/data/invalid_r4_patient.json", 'r').read()
-    # resource = open("../test/data/invalid_r4_patient.json", 'r').read()
+    validator: rito.Validator = rito.Validator.r4()
+    resource = open("../test/data/invalid_r4_patient.json", 'r').read()
 
-    # json_resource = json.loads(resource)
-    # output = validator.verbose_validate(json_resource)
-    # output = validator.dir_validate("../test/data/r4", verbose=True)
-    output = validator.file_validate("../test/data/invalid_r4_patient.json", verbose=True)
+    json_resource = json.loads(resource)
+    output = validator.verbose_validate(json_resource, 'invalid_r4_patient')
+    # output = validator.file_validate("../test/data/invalid_r4_patient.json", verbose=True)
 
     filename = "output" + datetime.now().strftime("%m%d-%H%M-%S") + ".txt"
     with open(filename, "w") as file:
